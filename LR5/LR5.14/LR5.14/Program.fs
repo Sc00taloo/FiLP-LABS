ca62b84e28241c876c99b56b779d438101a53db0
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-03-28 00:37:48 +0300

    LR5.14

---
+++ LR5/LR5.14/LR5.14/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let foon n foon1 init =
+    let rec foon2 x foon3 init prov =
+        if prov = 0 then init
+        else 
+            let newINIT = 
+                if x % prov = 0 then foon3 init prov 
+                else init
+            let newPROV = prov - 1
+            foon2 x foon3 newINIT newPROV
+    foon2 n foon1 init n
+    
+[<EntryPoint>]
+let main argv =
+    let n = System.Convert.ToInt32(Console.ReadLine())
+    foon n (fun x y -> x * y) 1 |> printfn "%A"
+    0
