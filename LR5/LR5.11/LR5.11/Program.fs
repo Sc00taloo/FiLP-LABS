3a4e18ddaced35d673f46302fc6ccd5d275b3699
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-03-27 16:21:42 +0300

    LR5.11

---
+++ LR5/LR5.11/LR5.11/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec Prov n =
+   if n = 1 || n = 2 then printfn "Ну ты и подлиза"
+   else printfn "Такое себе"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой язык проиграммирования у тебя любимый?"
+    printfn "Prolog - 1, F# - 2, C++ - 3, C# - 4, Pascal - 5, Python - 6"
+    System.Convert.ToInt32(System.Console.ReadLine()) |> Prov |> System.Console.WriteLine
+    0
