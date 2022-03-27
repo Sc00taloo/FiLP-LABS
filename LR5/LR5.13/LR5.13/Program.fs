160c82d3025581c842078870468225719f805214
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-03-27 22:58:19 +0300

    LR5.13

---
+++ LR5/LR5.13/LR5.13/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let BOOdiv value = (value / 10)
+let BOOprochent value = (value % 10)
+
+let rec DigitalSum value proiz =
+    let div = BOOdiv value
+    let prochent = BOOprochent value 
+    let sum = proiz * prochent
+    if div = 0 then sum
+    else DigitalSum div sum
+        
+
+[<EntryPoint>]
+let main argv =
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    DigitalSum n 1 |> printfn "%A" 
+    0 
