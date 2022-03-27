2dfd278fa3e4287166223c66576e9bafe1638577
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-03-28 01:57:30 +0300

    LR5.12

---
+++ LR5/LR5.12/LR5.12/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    //printfn "Какой язык проиграммирования у тебя любимый?"
+    //printfn "Prolog - 1, F# - 2, C++ - 3, C# - 4, Pascal - 5, Python - 6"
+    let test n =
+        match n with 
+        | "1" | "2" -> printfn "Ну ты и подлиза"
+        | _ -> printfn "Такое себе"
+
+    printfn "Какой язык проиграммирования у тебя любимый?"
+    printfn "Prolog - 1, F# - 2, C++ - 3, C# - 4, Pascal - 5, Python - 6"
+    (Console.ReadLine >> test >> Console.WriteLine)()
+
+    printfn "Какой язык проиграммирования у тебя любимый?"
+    printfn "Prolog - 1, F# - 2, C++ - 3, C# - 4, Pascal - 5, Python - 6"
+    let user input (output:string->unit) chooser = output (chooser (input ()))
+    user Console.ReadLine Console.WriteLine test
+    0
