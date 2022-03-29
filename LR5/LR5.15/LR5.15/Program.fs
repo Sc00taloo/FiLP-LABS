44acd86a2502ac43149a9039e72533d6722334b8
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-03-29 21:09:50 +0300

    LR5.15

---
+++ LR5/LR5.15/LR5.15/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+
+let rec prostoe a b =
+    if a = b then a
+    else
+        if (a > b) then 
+            let NEWA = b 
+            let NEWB = a-b
+            prostoe NEWA NEWB
+        else 
+            let NEWA = a 
+            let NEWB = b-a
+            prostoe NEWA NEWB
+            
+
+let foon n foon1 init =
+    let rec foon2 x foon3 init n = 
+        if n < 1 then init
+        else
+            let newInit = 
+                if prostoe x n = 1 then foon3 init n 
+                else init
+            let newN = n - 1
+            foon2 x foon3 newInit newN
+    foon2 n foon1 init n
+
+[<EntryPoint>]
+let main argv =
+    let n = System.Convert.ToInt32(Console.ReadLine())
+    foon n (fun x y -> x + y) 0 |> printfn "%A"
+    0 
