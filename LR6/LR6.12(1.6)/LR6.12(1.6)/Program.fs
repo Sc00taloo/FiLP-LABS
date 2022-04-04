a27f2730cde07e10f7ed817c8b7c2ae4f605af7c
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-04-04 23:02:15 +0300

    LR6.12(1.6)

---
+++ LR6/LR6.12(1.6)/LR6.12(1.6)/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+
+let rec ReadList n = 
+    if n=0 then []
+    else
+        let Head = System.Convert.ToInt32(System.Console.ReadLine())
+        let Tail = ReadList (n-1)
+        Head::Tail
+
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+    
+let sdvig_l (list: 'a list) =
+    if list.Length <= 1 then list
+    else
+        list.Tail @ [list.Head]
+
+let rec sdvig_left list n = 
+    if n = 0 then list
+    else
+        let new_n = n - 1
+        let new_list = sdvig_l list
+        sdvig_left new_list new_n
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Сколько элементов в списке?"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    let list = ReadList n
+
+    printfn "Новый список"
+    let sdvig = sdvig_left list 3
+    writeList sdvig
+    0 
