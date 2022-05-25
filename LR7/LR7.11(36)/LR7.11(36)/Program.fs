af7bb7383951dc2efdbb03dda4ddaed11271b80b
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-05-25 04:58:31 +0300

    LR7.11(36)

---
+++ LR7/LR7.11(36)/LR7.11(36)/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let rec ReadList n = 
+    if n = 0 then []
+    else
+        let Head = System.Convert.ToInt32(System.Console.ReadLine())
+        let Tail = ReadList (n-1)
+        Head::Tail
+
+let max (list: int list) =
+    let sorted = List.sortDescending list
+    List.tryFind (fun x -> x % 2 = 1) sorted
+
+[<EntryPoint>]
+let main argv =
+    printfn "Сколько элементов в списке?"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    let list = ReadList n
+
+    let maxNum = max list
+
+    if Option.isNone maxNum then printfn "Нет нечетных элементов"
+    else
+        printfn "Максимальный нечетный элемент: %d" (Option.get maxNum)
+    0
