c5a06ad706d4ca9a2770b1e02ae902007fbaa63f
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-05-25 06:00:32 +0300

    LR7.11(46)

---
+++ LR7/LR7.11(46)/LR7.11(46)/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let rec ReadList n = 
+    if n = 0 then []
+    else
+        let Head = System.Convert.ToInt32(System.Console.ReadLine())
+        let Tail = ReadList (n-1)
+        Head::Tail
+
+let rec WriteList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        WriteList tail
+
+[<EntryPoint>]
+let main argv =
+    printfn "Сколько элементов в списке?"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    let list = ReadList n
+    printfn "Положительные элементы:"
+    List.filter (fun x -> x >= 0) list |> WriteList
+    printfn "Отрицательные элементы:"
+    List.filter (fun x -> x < 0) list |> WriteList
+    0
