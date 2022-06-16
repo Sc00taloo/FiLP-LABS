4be27ec2272d13468c01f603bdfe1ee28afba2df
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-06-16 08:18:24 +0300

    LR12.13(Исправления)

--- LR12/LR12.13.pl
+++ LR12/LR12.13.pl
@@ -1,13 +1,13 @@
-fact(N, X) :- fact(N, X, 0, 1).
-fact(N, X, N, X) :- !.
-fact(N, X, N1, Res) :- N2 is N1 + 1, X2 is N2 * Res, fact(N, X, N2, X2).
+fact(0,F,F):-!.
+fact(I,F,Fact):-Fact1 is Fact * I, I1 is I - 1, fact(I1,F,Fact1).
+fact(N,X):-fact(N,X,1).
 
-digitSum(X, Sum) :- digitSum(X, Sum, 0).
-digitSum(0, Sum, Sum) :- !.
-digitSum(X, Sum, CurSum) :- Dig is X mod 10, fact(Dig, Dig1), NewSum is CurSum + Dig1, X1 is X div 10, digitSum(X1, NewSum, Sum).
+psfdin(0,X,X):-!.
+psfdin(N,X,Sum):-N1 is N div 10, Dig is N mod 10, fact(Dig,S), Sum1 is Sum + S, psfdin(N1,X,Sum1).
 
-is_WowNumber(X) :- digitSum(X, FactSum), X = FactSum.
+is_WowNumber(X):-psfdin(X,Sum,0), X is Sum.
 
-task13(X) :- task13(1000, 0, X).
-task13(2, X, X) :- !.
-task13(CurN, CurSum, X) :- NewN is CurN - 1, (is_WowNumber(CurN), NewSum is CurSum + CurN; NewSum is CurSum), task13(NewN, NewSum, X), !.
+task13(X):-task13(10000,X,0).
+task13(2,X,X):-!.
+task13(N,X,Sum):- is_WowNumber(N), Sum1 is Sum + N, N1 is N - 1, task13(N1,X,Sum1), !.
+task13(N,X,Sum):-N1 is N - 1, task13(N1,X,Sum).
